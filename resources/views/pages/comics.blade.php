d3d5a41ecb0b7eda2b6f72cfd61564068b86f10f
DiceglieA <angelodiceglie10@gmail.com>
2023-01-13 17:43:33 +0100

    css collegato

--- resources/views/pages/comics.blade.php
+++ resources/views/pages/comics.blade.php
@@ -2,8 +2,19 @@
 @section('title', 'comics')
 @section('main-content')
     <ul class="container">
-        @foreach ($comics as $elem)
-            <li>{{ $elem['title'] }}</li>
-        @endforeach
+        <div class="row">
+            @foreach ($comics as $comic)
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title">{{ $comic['title'] }}</h5>
+                        <p class="card-text">{{ $comic['description'] }}</p>
+                        <a href="#" class="btn btn-primary">Apri fumetto</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </ul>
 @endsection
